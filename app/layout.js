3d5e3e42a91b8e78a3bfc132cf2d0cfdaccf91fb
remindingmeofyouu <wyd7xpd.1@gmail.com>
2026-04-11 22:25:34 -0400

    add scythe favicon

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
   title: "fate.rip | Home",
   description: "Claim your unique profile link today.",
   icons: {
-    icon: "https://media.discordapp.net/attachments/1492245469785755840/1492688630685106347/scythe.png?ex=69dc3e1e&is=69daec9e&hm=ff8a4b2ed25f7a8b663564ff4c1526d69b7469e999a7e8ea4722dfae6b2c640e&=&format=webp&quality=lossless",
+   icon: "/scythe.png",
   },
 };
 
