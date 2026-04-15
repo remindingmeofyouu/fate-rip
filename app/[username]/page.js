a56eeca22f7fa5dbf3dae256b354cb78b221c8c2
remindingmeofyouu <wyd7xpd.1@gmail.com>
2026-04-15 18:33:07 -0400

    fix supabase import path

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState, useRef } from 'react'
 import { useParams } from 'next/navigation'
-import { supabase } from '../../lib/supabase'
+import { supabase } from '../lib/supabase'
 
 export default function ProfilePage() {
   const params = useParams()
